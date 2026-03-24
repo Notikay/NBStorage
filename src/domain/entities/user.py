496ada76773b543591860e0a29a882273531cc31
Notikay <a.bob_bis@mail.ru>
2026-03-24 13:15:15 +0500

    REF error message and docstring for time block
    Изменение сообщения об ошибке и описание этой ошибки, во время обработки времени блокировки сессии, после инициализации настроек пользователя.
    Задача: User-4

--- src/domain/entities/user.py
+++ src/domain/entities/user.py
@@ -69,7 +69,8 @@ class Settings(AbstractData[SettingsDTO]):
         :raises ValueError: Если имя пользователя пустое.
                             Если аватарка пользователя имеет
                             неподдерживаемый формат.
-                            Если время блокировки отрицательное.
+                            Если время блокировки сессии пользователя
+                            отрицательное.
         """
         if not self._name:
             raise ValueError("Имя пользователя не должно быть пустым!")
@@ -77,8 +78,8 @@ class Settings(AbstractData[SettingsDTO]):
             raise ValueError("Аватарка пользователя имеет неподдерживаемый "
                              "формат!")
         elif self._time_block < 0:
-            raise ValueError("Время блокировки не может быть отрицательным!")
-
+            raise ValueError("Время блокировки сессии пользователя не может "
+                             "быть отрицательным!")
 
 @dataclass(slots=True)
 class User(AbstractData[UserDTO]):
